45b6c6eea4e777db97fe269f38021f556f30a66e
yhogann <161995031+yhogann@users.noreply.github.com>
2025-09-13 08:57:05 -0500

    Se crea clase rectangulo

---
+++ rectangulo.py
@@ -0,0 +1,37 @@
+from figuraGeometrica import FiguraGeometrica
+from excepciones import ValorGeometricoInvalido
+
+class rectangulo(FiguraGeometrica):
+    def __init__(self, base: float, altura: float):
+        super().__init__(lados=4)      
+        self.base = base
+        self.altura = altura             
+
+    @property
+    def getBase(self) -> float:
+        return self._base
+
+    @base.setter
+    def base(self, valor):
+        if valor <= 0:
+            raise ValorGeometricoInvalido("La base debe ser positivo.")
+        self._base = float(valor)
+
+    @property
+    def getAltura(self) -> float:
+        return self._altura
+
+    @altura.setter
+    def altura(self, valor):
+        if valor <= 0:
+            raise ValorGeometricoInvalido("La altura debe ser positivo.")
+        self._altura = float(valor)
+
+    def area(self) -> float:
+        return (self.__base * self.__altura)/2
+
+    def perimetro(self) -> float:
+        return 2 * (self.__base * self.__altura)
+
+    def __str__(self) -> str:
+        return f"Rectangulo(base={self.base}, altura={self.altura})"
